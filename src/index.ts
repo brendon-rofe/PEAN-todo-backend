7393b8138c899f72331fadfb1f42e0acfa9e82f0
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-10-02 16:18:16 +0200

    feat: import prisma client and return all todos

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express, { Request, Response } from 'express'
+import { PrismaClient } from '../generated/prisma';
 
-const app = express()
+const app = express();
+const prisma = new PrismaClient();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/all', (req: Request, res: Response) => {
-  res.send();
+app.get('/all', async (req: Request, res: Response) => {
+  const allTodos = await prisma.todo.findMany();
+  res.json(allTodos);
 })
 
 app.listen(port, () => {
