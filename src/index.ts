9f8870936dccbcfa193aa01f175ca12f92720152
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-10-03 13:09:17 +0200

    feat: Create create todo endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from 'express';
 import { PrismaClient } from '../generated/prisma';
 
 const app = express();
@@ -7,11 +7,22 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.post('/create', async (req: Request, res: Response) => {
+  const newTodo = req.body;
+  const createdTodo = await prisma.todo.create({
+    data: {
+      description: newTodo.description,
+      done: false
+    },
+  });
+  res.json(createdTodo);
+});
+
 app.get('/all', async (req: Request, res: Response) => {
   const allTodos = await prisma.todo.findMany();
   res.json(allTodos);
-})
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
-})
+});
